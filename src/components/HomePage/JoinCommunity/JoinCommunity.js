89c700d02b02b72124c3d946a4d495f561bef74e
Macbook <macbook@MacBook-Pro-Macbook-2.local>
2022-04-04 23:31:51 +0200

    stylization community section

--- src/components/HomePage/JoinCommunity/JoinCommunity.js
+++ src/components/HomePage/JoinCommunity/JoinCommunity.js
@@ -1,24 +1,30 @@
 import React from "react"
+import arrow from "../../../images/follow-arrow.svg"
 import * as content from "../../../constants/content/joinCommunity"
 import * as styles  from "./JoinCommunity.module.css"
 
 const JoinCommunity = () => (
-  <div className={"container"}>
-    <div className={styles.communityWrap}>
-      <div className={styles.communityContent}>
+  <div className={styles.communityWrap}>
+    <div className={"container"}>
         <div className={styles.titleWrap}>
           <h2>{content.TITLE} <span>{content.TITLE_COLORED}</span></h2>
           <p>{content.DESCRIPTION}</p>
         </div>
-        {content.SOCIAL_LIST.map((el,idx) => (
-          <div key={idx}>
-            <div>{el.icon}</div>
-            <div>{el.title}</div>
-            <div>{el.text}</div>
-            <a href="/">{el.btn_text}</a>
-          </div>
-        ))}
-      </div>
+        <div className={styles.communitiesList}>
+          {content.SOCIAL_LIST.map((el,idx) => (
+            <div className={styles.community} key={idx}>
+              <div className={styles.communityContent}>
+                <div className={styles.icon}>{el.icon}</div>
+                <div className={styles.title}>{el.title}</div>
+                <div className={styles.text}>{el.text}</div>
+                <a href="/" className={styles.link}>
+                  {el.btn_text}
+                  <img src={arrow} alt="follow"/>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   </div>
 )
