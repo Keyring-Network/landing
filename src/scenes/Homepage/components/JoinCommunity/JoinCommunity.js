244c7997bda2721109e1105d7b674056becc71bf
BorkovskayaAnna <borkovskayaa23@gmail.com>
2022-04-08 18:22:40 +0200

    add decor elements

--- src/scenes/Homepage/components/JoinCommunity/JoinCommunity.js
+++ src/scenes/Homepage/components/JoinCommunity/JoinCommunity.js
@@ -1,30 +1,35 @@
 import React from "react"
+import classNames from "classnames/bind"
 import arrow from "../../../../assets/images/follow-arrow.svg"
+import decor1 from "../../../../assets/images/decor/padlock-mirror.svg"
+import decor2 from "../../../../assets/images/decor/key.svg"
 import * as content from "../../../../constants/content/joinCommunity"
 import * as styles  from "./JoinCommunity.module.css"
 
 const JoinCommunity = () => (
   <div id={"community"} className={styles.communityWrap}>
-    <div className={"container"}>
-        <div className={styles.titleWrap}>
-          <h2>{content.TITLE} <span>{content.TITLE_COLORED}</span></h2>
-          <p>{content.DESCRIPTION}</p>
-        </div>
-        <div className={styles.communitiesList}>
-          {content.SOCIAL_LIST.map((el, idx) => (
-            <div className={styles.community} key={idx}>
-              <div className={styles.communityContent}>
-                <div className={styles.icon}>{el.icon}</div>
-                <div className={styles.title}>{el.title}</div>
-                <div className={styles.text}>{el.text}</div>
-                <a href="/" className={styles.link}>
-                  {el.btn_text}
-                  <img src={arrow} alt="follow"/>
-                </a>
-              </div>
+    <div className={classNames("container", styles.communityContainer)}>
+      <img src={decor1} className={styles.decorPadlock} alt={"diagram"} />
+      <img src={decor2} className={styles.decorKey} alt={"diagram"} />
+      <div className={styles.titleWrap}>
+        <h2>{content.TITLE} <span>{content.TITLE_COLORED}</span></h2>
+        <p>{content.DESCRIPTION}</p>
+      </div>
+      <div className={styles.communitiesList}>
+        {content.SOCIAL_LIST.map((el, idx) => (
+          <div className={styles.community} key={idx}>
+            <div className={styles.communityContent}>
+              <div className={styles.icon}>{el.icon}</div>
+              <div className={styles.title}>{el.title}</div>
+              <div className={styles.text}>{el.text}</div>
+              <a href="/" className={styles.link}>
+                {el.btn_text}
+                <img src={arrow} alt="follow"/>
+              </a>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   </div>
 )
