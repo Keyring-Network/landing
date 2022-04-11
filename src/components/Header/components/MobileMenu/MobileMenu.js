e87b52a93a51649f3a781200072e4dc9837c3378
BorkovskayaAnna <borkovskayaa23@gmail.com>
2022-04-11 15:42:27 +0200

    styles fixes

--- src/components/Header/components/MobileMenu/MobileMenu.js
+++ src/components/Header/components/MobileMenu/MobileMenu.js
@@ -3,30 +3,26 @@ import { Link } from "gatsby"
 import classNames from "classnames/bind"
 import * as styles from "./MobileMenu.module.css"
 
-const MobileMenu = ({menuList, open, onCLickMenu}) => {
-  // const [isOpen, setIsOpen] = useState(false)
-
-  return (
-    <>
-      <div
-        className={classNames(styles.hamburger, open && styles.hamburgerOpen)}
-        onClick={onCLickMenu}
-      >
-        <span />
-        <span />
-        <span />
-      </div>
-      {open &&
-        <ul className={styles.mobileMenu}>
-          {menuList.map((el, idx) => (
-            <li key={idx}>
-              <Link to={el.url}>{el.text}</Link>
-            </li>
-          ))}
-        </ul>
-      }
-    </>
-  )
-}
+const MobileMenu = ({menuList, open, onCLickMenu}) => (
+  <>
+    <div
+      className={classNames(styles.hamburger, open && styles.hamburgerOpen)}
+      onClick={onCLickMenu}
+    >
+      <span />
+      <span />
+      <span />
+    </div>
+    {open &&
+      <ul className={styles.mobileMenu}>
+        {menuList.map((el, idx) => (
+          <li key={idx}>
+            <Link to={el.url}>{el.text}</Link>
+          </li>
+        ))}
+      </ul>
+    }
+  </>
+)
 
 export default MobileMenu
