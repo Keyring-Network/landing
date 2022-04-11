b31817da2059d400271eed5d3a8d7a46ca824928
MityaDementiy <d.dementiy@gmail.com>
2022-04-11 18:51:22 +0300

    Use gatsby image in Hero

--- src/scenes/Homepage/components/Hero/Hero.js
+++ src/scenes/Homepage/components/Hero/Hero.js
@@ -1,37 +1,35 @@
-import React from "react"
-import classNames from "classnames/bind"
-import * as styles  from "./Hero.module.css"
-import * as content from "../../../../constants/content/hero"
-import Button from "../../../../components/Button"
-import heroAnon from "../../../../assets/images/hero-anon.png"
-import padlock from "../../../../assets/images/padlock.svg"
-import key from "../../../../assets/images/key.svg"
-import anonMask from "../../../../assets/images/anon-mask.svg"
-import shield from "../../../../assets/images/shield.svg"
+import React from 'react';
+import classNames from 'classnames/bind';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import * as styles from './Hero.module.css';
+import * as content from '../../../../constants/content/hero';
+import Button from '../../../../components/Button';
 
 const Hero = () => (
   <div className={styles.hero}>
-    <div className={"container"}>
+    <div className="container">
       <div className={styles.heroText}>
-        <h1>{content.TITLE}
+        <h1>
+          {content.TITLE}
           <span>{content.TITLE_COLORED}</span>
           {content.TITLE_LAST}
         </h1>
         <p className={styles.description}>{content.DESCRIPTION}</p>
         <Button
           url={content.CTA_BUTTON_URL}
-          btnStyles={classNames("button b-green", styles.heroBtn)}
+          btnStyles={classNames('button b-green', styles.heroBtn)}
           title={content.CTA_BUTTON}
         />
 
         <p className={styles.ctaText}>{content.CTA_TEXT}</p>
-        <img src={padlock} className={styles.padlock} alt="padlock" />
-        <img src={key} className={styles.key} alt="key" />
-        <img src={anonMask} className={styles.anonMask} alt="anon mask" />
-        <img src={shield} className={styles.shield} alt="shield" />
+        <StaticImage src="../../../../assets/images/padlock.svg" className={styles.padlock} alt="padlock" placeholder="blurred" loading="eager" />
+        <StaticImage src="../../../../assets/images/key.svg" className={styles.key} alt="key" placeholder="blurred" loading="eager" />
+        <StaticImage src="../../../../assets/images/anon-mask.svg" className={styles.anonMask} alt="anon mask" placeholder="blurred" loading="eager" />
+        <StaticImage src="../../../../assets/images/shield.svg" className={styles.shield} alt="shield" placeholder="blurred" loading="eager" />
       </div>
     </div>
   </div>
-)
+);
 
-export default Hero
+export default Hero;
