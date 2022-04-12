0ffeec77cad753cb7f0cf2a483d26c7570d2bd8d
MityaDementiy <d.dementiy@gmail.com>
2022-04-12 09:50:20 +0300

    Defer offscreen images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,51 +1,51 @@
-import React from "react"
-import { Link } from "gatsby"
-import classNames from "classnames/bind"
-import logo from "../../assets/images/logo.svg"
-import * as menu from "../../constants/navMenu"
-import * as content from "../../constants/footer"
-import * as styles from "./Footer.module.css"
+import React from 'react';
+import { Link } from 'gatsby';
+import classNames from 'classnames/bind';
+import logo from '../../assets/images/logo.svg';
+import * as menu from '../../constants/navMenu';
+import * as content from '../../constants/footer';
+import * as styles from './Footer.module.css';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div className={classNames("container", styles.footerContainer)}>
-      <Link to={"/"}>
-        <img src={logo} alt={"keyring logo"} />
+    <div className={classNames('container', styles.footerContainer)}>
+      <Link to="/">
+        <img src={logo} alt="keyring logo" loading="lazy" />
       </Link>
       <ul className={styles.menu}>
-        {menu.navMenu.map((el, idx) => (
-          <li key={idx}>
+        {menu.navMenu.map((el) => (
+          <li key={el.url}>
             <Link to={el.url}>{el.text}</Link>
           </li>
         ))}
       </ul>
     </div>
-    <div className={classNames("container", styles.footerContainer)}>
+    <div className={classNames('container', styles.footerContainer)}>
       <p className={styles.footerInfo}>{content.INFO_TEXT}</p>
       <ul className={styles.mobileMenu}>
-        {menu.navMenu.map((el, idx) => (
-          <li key={idx}>
+        {menu.navMenu.map((el) => (
+          <li key={el.url}>
             <Link to={el.url}>{el.text}</Link>
           </li>
         ))}
       </ul>
       <ul className={styles.socials}>
-        {content.SOCIAL.map((el, idx) => (
-          <li key={idx}>
-            <a href={el.url} target={"_blank"}>
-              <img src={el.icon} alt="social icon"/>
+        {content.SOCIAL.map((el) => (
+          <li key={el.icon}>
+            <a href={el.url} target="_blank" rel="noreferrer">
+              <img src={el.icon} alt="social icon" loading="lazy" />
             </a>
           </li>
         ))}
       </ul>
     </div>
-    <div className={classNames("container", styles.footerContainer)}>
+    <div className={classNames('container', styles.footerContainer)}>
       <hr />
       <p className={styles.rights}>{content.RIGHTS_RESERVED_TEXT}</p>
       <ul className={styles.termsMenu}>
-        {content.TERMS_MENU.map((el, idx) => (
-          <li key={idx}>
-            <a href={el.url} title={el.text} target={"_blank"}>
+        {content.TERMS_MENU.map((el) => (
+          <li key={el.text}>
+            <a href={el.url} title={el.text} target="_blank" rel="noreferrer">
               {el.text}
             </a>
           </li>
@@ -53,6 +53,6 @@ const Footer = () => (
       </ul>
     </div>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
