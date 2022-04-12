93ed8bc01d869d1a993cb880df4f8808ec77de56
MityaDementiy <d.dementiy@gmail.com>
2022-04-12 09:17:58 +0300

    Defer loading of offscreen images

--- src/scenes/Homepage/components/WhatWeDo/WhatWeDo.js
+++ src/scenes/Homepage/components/WhatWeDo/WhatWeDo.js
@@ -1,32 +1,40 @@
-import React, {useState} from "react"
-import classNames from "classnames/bind"
-import decor1 from "../../../../assets/images/decor/arrow.svg"
-import decor2 from "../../../../assets/images/decor/padlock.svg"
-import * as content from "../../../../constants/content/whatWeDo"
-import * as styles  from "./WhatWeDo.module.css"
+import React, { useState } from 'react';
+import classNames from 'classnames/bind';
+import decor1 from '../../../../assets/images/decor/arrow.svg';
+import decor2 from '../../../../assets/images/decor/padlock.svg';
+import * as content from '../../../../constants/content/whatWeDo';
+import * as styles from './WhatWeDo.module.css';
 
 const WhatWeDo = () => {
-  const [activeTab, setActiveTab] = useState(0)
+  const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div id={"how-works"} className={styles.partnersWrap}>
-      <div className={classNames("container", styles.container)}>
+    <div id="how-works" className={styles.partnersWrap}>
+      <div className={classNames('container', styles.container)}>
         <div className={styles.titleWrap}>
-          <h2>{content.TITLE} <span>{content.TITLE_COLORED}</span></h2>
+          <h2>
+            {content.TITLE}
+            {' '}
+            <span>{content.TITLE_COLORED}</span>
+          </h2>
           <p>{content.DESCRIPTION}</p>
         </div>
         <div className={styles.listWrap}>
-          {content.LIST.map((el,idx) => (
+          {content.LIST.map((el, idx) => (
             <div
               className={classNames(styles.itemListWrap, activeTab === idx ? styles.active : '')}
-              onClick={() => {setActiveTab(idx)}}
-              key={idx}
+              onClick={() => { setActiveTab(idx); }}
+              onKeyDown={() => { setActiveTab(idx); }}
+              // eslint-disable-next-line react/no-array-index-key
+              key={`${el.title}, ${idx}`}
+              role="button"
+              tabIndex="0"
             >
               <div className={classNames(styles.itemListImageWrap)}>
                 <div className={classNames(styles.itemListImage)}>
-                  <img src={el.img} className={styles.mainImg} alt={"diagram"} />
-                  <img src={decor1} className={styles.decorArrow} alt={"diagram"} />
-                  <img src={decor2} className={styles.decorLock} alt={"diagram"} />
+                  <img src={el.img} className={styles.mainImg} alt="diagram" loading="lazy" />
+                  <img src={decor1} className={styles.decorArrow} alt="diagram" loading="lazy" />
+                  <img src={decor2} className={styles.decorLock} alt="diagram" loading="lazy" />
                 </div>
               </div>
               <div className={classNames(styles.itemList)}>
@@ -41,7 +49,7 @@ const WhatWeDo = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhatWeDo
+export default WhatWeDo;
