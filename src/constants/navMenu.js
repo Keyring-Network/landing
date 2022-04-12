745e4482eafe43c75568a79a96f918212e8d6202
MityaDementiy <d.dementiy@gmail.com>
2022-04-12 13:13:19 +0300

    Fix header

--- src/constants/navMenu.js
+++ src/constants/navMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const navMenu = [
-  { text: 'How It Works', url: '#how-works' },
+  { text: 'What We Do', url: '#how-works' },
   { text: 'News', url: '#news' },
   { text: 'FAQ', url: '#faq' },
   { text: 'Community', url: '#community' },
