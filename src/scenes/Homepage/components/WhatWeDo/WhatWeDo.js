8295a67ea2830e19743883fcde301f37b5946423
BorkovskayaAnna <borkovskayaa23@gmail.com>
2022-04-13 15:34:03 +0200

    rewrite section functionality

--- src/scenes/Homepage/components/WhatWeDo/WhatWeDo.js
+++ src/scenes/Homepage/components/WhatWeDo/WhatWeDo.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import classNames from 'classnames/bind';
+import mainImg from "../../../../assets/images/what_we_do_1.jpg"
 import decor1 from '../../../../assets/images/decor/arrow.svg';
 import decor2 from '../../../../assets/images/decor/padlock.svg';
 import * as content from '../../../../constants/content/whatWeDo';
@@ -11,45 +12,56 @@ const WhatWeDo = () => {
   return (
     <div id="how-works" className={styles.partnersWrap}>
       <div className={classNames('container', styles.container)}>
-        <div className={styles.titleWrap}>
-          <h2>
-            {content.TITLE}
-            {' '}
-            <span>{content.TITLE_COLORED}</span>
-          </h2>
-          <p>{content.DESCRIPTION}</p>
-        </div>
-        <div className={styles.listWrap}>
-          {content.LIST.map((el, idx) => (
+        <div className={styles.contentWrap}>
+          <div className={classNames(styles.imageWrap)}>
             <div
-              className={classNames(styles.itemListWrap, activeTab === idx ? styles.active : '')}
-              onClick={() => { setActiveTab(idx); }}
-              onKeyDown={() => { setActiveTab(idx); }}
-              // eslint-disable-next-line react/no-array-index-key
-              key={`${el.title}, ${idx}`}
-              role="button"
-              tabIndex="0"
-              // style={{ backgroundImage: `url(${el.img})`}}
+              className={classNames(styles.image)}
+              style={{ backgroundImage: `url(${mainImg})` }}
             >
-              <div className={classNames(styles.itemListImageWrap)}>
-                <div
-                  className={classNames(styles.itemListImage)}
-                   style={{ backgroundImage: `url(${el.img})` }}
-                >
-                  {/*<img src={el.img} className={styles.mainImg} alt="diagram" loading="lazy" />*/}
-                  <img src={decor1} className={styles.decorArrow} alt="decor icon" loading="lazy" />
-                  <img src={decor2} className={styles.decorLock} alt="decor icon" loading="lazy" />
-                </div>
-              </div>
-              <div className={classNames(styles.itemList)}>
-                <div className={styles.icon}>{el.icon}</div>
-                <div className={classNames(styles.itemListContent)}>
-                  <p className={styles.title}>{el.title}</p>
-                  <p className={styles.text}>{el.text}</p>
-                </div>
+              <img src={decor1} className={styles.decorArrow} alt="decor icon" loading="lazy" />
+              <img src={decor2} className={styles.decorLock} alt="decor icon" loading="lazy" />
+            </div>
+          </div>
+          <div>
+            <div className={styles.titleWrap}>
+              <h2>
+                {content.TITLE}
+                {' '}
+                <span>{content.TITLE_COLORED}</span>
+              </h2>
+              <p>{content.DESCRIPTION}</p>
+            </div>
+            <div className={classNames(styles.imageWrap, styles.imageWrapMobile)}>
+              <div
+                className={classNames(styles.image)}
+                style={{ backgroundImage: `url(${mainImg})` }}
+              >
+                <img src={decor1} className={styles.decorArrow} alt="decor icon" loading="lazy" />
+                <img src={decor2} className={styles.decorLock} alt="decor icon" loading="lazy" />
               </div>
             </div>
-          ))}
+            <div className={styles.listWrap}>
+              {content.LIST.map((el, idx) => (
+                <div
+                  className={classNames(styles.itemListWrap, activeTab === idx ? styles.active : '')}
+                  onClick={() => { setActiveTab(idx); }}
+                  onKeyDown={() => { setActiveTab(idx); }}
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={`${el.title}, ${idx}`}
+                  role="button"
+                  tabIndex="0"
+                >
+                  <div className={classNames(styles.itemList)}>
+                    <div className={styles.icon}>{el.icon}</div>
+                    <div className={classNames(styles.itemListContent)}>
+                      <p className={styles.title}>{el.title}</p>
+                      <p className={styles.text}>{el.text}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
