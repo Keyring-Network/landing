d19c3204614f98879dcc5363146baccf28631497
squarefunction <102609421+squarefunction@users.noreply.github.com>
2022-04-21 23:47:14 +0100

    Update hero.js

--- src/constants/content/hero.js
+++ src/constants/content/hero.js
@@ -6,7 +6,7 @@ export const TITLE = (
   </>
 );
 export const DESCRIPTION =
-  "Access verified TradFi-backed trading pools with Keyring's protocol-agnostic whitelisting and AML/KYC.";
+  "Access verified TradFi-backed trading pools with protocol-agnostic whitelisting and AML/KYC";
 export const CTA_TEXT = "Keep your identity in complete anonymity";
 export const CTA_BUTTON = "Join the Waitlist";
 export const CTA_BUTTON_URL = "https://0d07oori297.typeform.com/to/PF5a05ot";
