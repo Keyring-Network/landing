7233f2cf9a4327593c6b40cd4e0e3be82d9166e3
Alex <flipdazed@gmail.com>
2022-04-24 08:08:26 +0200

    removed FAQ dead link

--- src/constants/navMenu.js
+++ src/constants/navMenu.js
@@ -2,7 +2,7 @@
 export const navMenu = [
   { text: 'What We Do', url: '#how-works' },
   // { text: 'News', url: '#news' },
-  { text: 'FAQ', url: '#faq' },
+  // { text: 'FAQ', url: '#faq' },
   // { text: 'Community', url: '#community' },
   { text: 'Join the Waitlist', url: '#join-waitlist' },
 ];
