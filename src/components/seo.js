7310f0220ec656ada4d6dc8ad6504034ad2b236c
OxMarco <>
2022-05-10 16:55:42 +0200

    seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,13 +9,13 @@
  import PropTypes from "prop-types"
  import { Helmet } from "react-helmet"
  import { useStaticQuery, graphql } from "gatsby"
-
+ 
  import favicon from "../assets/images/favicon/favicon.ico"
  import favicon32 from "../assets/images/favicon/favicon-32x32.png"
  import favicon16 from "../assets/images/favicon/favicon-16x16.png"
  import favicon96 from "../assets/images/favicon/favicon-96x96.png"
  import favicon72 from "../assets/images/favicon/apple-icon-72x72.png"
-
+ 
  function SEO({ title, description, image, meta }) {
    const { site } = useStaticQuery(
      graphql`
@@ -35,7 +35,7 @@
      '@context': 'http://schema.org',
          '@type': 'Corporation',
          '@id': 'https://keyring.network/',
-         'name': 'Keyring Network',
+         'name': 'Keyring',
          'alternateName': 'Keyring Network',
          'url': 'https://keyring.network/',
          'email': 'info@keyring.network',
@@ -67,6 +67,49 @@
    const metaDescription = description || site.siteMetadata.description
    const canonical = site.siteMetadata.siteUrl
  
+   const metaTags = [
+     {
+       name: `description`,
+       content: metaDescription,
+     },
+     {
+       property: `og:title`,
+       content: defaultTitle,
+     },
+     {
+       name: `og:image`,
+       content: image,
+     },
+     {
+       property: `og:description`,
+       content: metaDescription,
+     },
+     {
+       property: `og:type`,
+       content: `website`,
+     },
+     {
+       property: `og:locale`,
+       content: lang,
+     },
+     {
+       name: `twitter:card`,
+       content: `summary`,
+     },
+     {
+       name: `twitter:title`,
+       content: defaultTitle,
+     },
+     {
+       name: `twitter:image`,
+       content: image,
+     },
+     {
+       name: `twitter:description`,
+       content: metaDescription,
+     },
+   ].concat(meta || [])
+ 
    return (
      <Helmet
        htmlAttributes={{
@@ -83,58 +126,20 @@
              ]
            : []
        }
-       meta={[
-         {
-           name: `description`,
-           content: metaDescription,
-         },
-         {
-           property: `og:title`,
-           content: defaultTitle,
-         },
-         {
-           name: `og:image`,
-           content: image,
-         },
-         {
-           property: `og:description`,
-           content: metaDescription,
-         },
-         {
-           property: `og:type`,
-           content: `website`,
-         },
-         {
-           property: `og:locale`,
-           content: lang,
-         },
-         {
-           name: `twitter:card`,
-           content: `summary`,
-         },
-         {
-           name: `twitter:title`,
-           content: defaultTitle,
-         },
-         {
-           name: `twitter:image`,
-           content: image,
-         },
-         {
-           name: `twitter:description`,
-           content: metaDescription,
-         },
-       ].concat(meta)}
      >
-        <script type="application/ld+json">{JSON.stringify(structuredContent)}</script>
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel={"apple-touch-icon"} sizes={"72x72"} href={favicon72} />
-        <link rel={"icon"} type={"image/png"} sizes={"32x32"} href={favicon32}/>
-        <link rel={"icon"} type={"image/png"} sizes={"96x96"} href={favicon96}/>
-        <link rel={"icon"} type={"image/png"} sizes={"96x96"} href={favicon96}/>
-        <link rel={"icon"} type={"image/png"} sizes={"16x16"} href={favicon16}/>
-        <link rel={"icon"} href={favicon}/>
+       <script type="application/ld+json">{JSON.stringify(structuredContent)}</script>
+       <meta name="msapplication-TileColor" content="#151A29" />
+       <meta name="theme-color" content="#151A29" />
+       <link rel={"apple-touch-icon"} sizes={"72x72"} href={favicon72} />
+       <link rel={"icon"} type={"image/png"} sizes={"32x32"} href={favicon32}/>
+       <link rel={"icon"} type={"image/png"} sizes={"96x96"} href={favicon96}/>
+       <link rel={"icon"} type={"image/png"} sizes={"96x96"} href={favicon96}/>
+       <link rel={"icon"} type={"image/png"} sizes={"16x16"} href={favicon16}/>
+       <link rel={"icon"} href={favicon}/>
+       {metaTags.map((meta) =>  {
+           return (<meta key={meta.name} name={meta.name} content={meta.content} />)
+         }
+       )}
      </Helmet>
    )
  }
