e610e5f75027cdf389fb88f55ce7913267fdd6e6
squarefunction <102609421+squarefunction@users.noreply.github.com>
2022-05-20 11:05:51 +0100

    Update hero.js

--- src/constants/content/hero.js
+++ src/constants/content/hero.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export const TITLE = (
   <>
-    <span>for</span> DeFi; 
-    <span> from</span> DeFi
+    <span>Connecting</span> DeFi; 
+    <span> x</span> TradFi
   </>
 );
 export const DESCRIPTION =
-  "On-chain, privacy preserving AML/KYC for smart contracts. Connecting DeFi x TradFi";
+  "On-chain, privacy preserving AML/KYC for smart contracts. For DeFi, From DeFi";
 export const CTA_TEXT = "Keep your identity in complete anonymity";
 export const CTA_BUTTON = "Join the Waitlist";
 export const CTA_BUTTON_URL = "https://0d07oori297.typeform.com/to/PF5a05ot";
