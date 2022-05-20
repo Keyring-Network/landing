70025d1524d0be10bf5727ed5d66e974da5250f3
squarefunction <102609421+squarefunction@users.noreply.github.com>
2022-05-20 13:52:11 +0100

    Update hero.js

--- src/constants/content/hero.js
+++ src/constants/content/hero.js
@@ -7,7 +7,7 @@ export const TITLE = (
   </>
 );
 export const DESCRIPTION =
-  "On-chain, privacy preserving AML/KYC for smart contracts. For DeFi, From DeFi";
+  "On-chain, privacy preserving AML/KYC for smart contracts. For DeFi, from DeFi";
 export const CTA_TEXT = "Keep your identity in complete anonymity";
 export const CTA_BUTTON = "Join the Waitlist";
 export const CTA_BUTTON_URL = "https://0d07oori297.typeform.com/to/PF5a05ot";
