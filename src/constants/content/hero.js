11e8083a6acc1f2f3f6a9fe536f190387668ebbe
squarefunction <102609421+squarefunction@users.noreply.github.com>
2022-05-27 16:39:36 +0100

    Update hero.js

--- src/constants/content/hero.js
+++ src/constants/content/hero.js
@@ -2,17 +2,14 @@ import React from 'react';
 
 export const TITLE = (
   <>
-    Seamless,
-    <br />
-    Privacy-Preserving,
-    <br />
-    <span>Whitelisting</span>
-    <br />
-    for DeFi
+    <span>Connecting </span><br />
+    DeFi
+    <span> x </span>
+    TradFi
   </>
 );
 export const DESCRIPTION =
-  "Access verified TradFi-backed trading pools with Keyring's protocol-agnostic whitelisting and AML/KYC.";
+  "On-chain, privacy-preserving AML/KYC for smart contracts. For DeFi; from DeFi.";
 export const CTA_TEXT = 'Keep your identity in complete anonymity';
 export const CTA_BUTTON = 'Join the Waitlist';
 export const CTA_BUTTON_URL = 'https://0d07oori297.typeform.com/to/PF5a05ot';
